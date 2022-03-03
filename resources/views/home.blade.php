97f732ac5beda366214b07e315cbf0a13ba79a7c
KCorigliano <corigliano98@hotmail.com>
2022-03-03 17:23:18 +0100

    footer with links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,56 @@
 @extends('layout.default')
 
 @section('main')
+<div class="container-fluid comics-background">
+    <div class="container comics-container">
+        <span class="series-info">CURRENT SERIES</span>
+    </div>
+</div>
+<div class="container-fluid shop-nav flex-container">
+    <div class="container">
+        <div class="flex-container align-items-center">
+            <div class="col shops-container my-auto align-items-center">
+                <div class="flex-container">
+                    <div class="shop-img-container pe-2">
+                        <img class="img-fluid" src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <span>DIGITAL COMICS</span>
+                </div>
+            </div>
+            <div class="col shops-container my-auto">
+                <div class="flex-container">
+                    <div class="shop-img-container pe-2">
+                        <img class="img-fluid" src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+                    </div>
+                    <span>DC MERCHANDISE</span>
+                </div>
+            </div>
+            <div class="col shops-container my-auto">
+                <div class="flex-container">
+                    <div class="shop-img-container pe-2">
+                        <img class="img-fluid" src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+                    </div>
+                    <span>SUBSCRIPTION</span>
+                </div>
+            </div>
+            <div class="col shops-container my-auto">
+                <div class="flex-container">
+                    <div class="shop-img-container pe-2">
+                        <img class="img-fluid" src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+                    </div>
+                    <span>COMIC SHOP LOCATION</span>
+                </div>
+            </div>
+            <div class="col shops-container my-auto">
+                <div class="flex-container">
+                    <div class="shop-img-container pe-2">
+                        <img class="img-fluid" src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+                    </div>
+                    <span>DC POWER VISA</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -31,12 +31,56 @@
                 </div>
             </div>
         </div>
+        <div class="jumbotron">
+        </div>
     </header>
     <main>
         @yield('main')
     </main>
     <footer>
-        @yield('footer')
+        <div class="container-fluid">
+            <div class="container">
+                <div class="footer-left">
+                    <div>
+                        <h3>DC COMICS</h3>
+                        <p>Characters</p>
+                        <p>Comics</p>
+                        <p>Movies</p>
+                        <p>TV</p>
+                        <p>Games</p>
+                        <p>Videos</p>
+                        <p>News</p>
+                        <h3>SHOP</h3>
+                        <p>Shop DC</p>
+                        <p>Shop DC Collectables</p>
+                    </div>
+                    <div>
+                        <h3>DC</h3>
+                        <p>Termo of use</p>
+                        <p>Privacy policy (New)</p>
+                        <p>Ad Choices</p>
+                        <p>Adverstising</p>
+                        <p>Jobs</p>
+                        <p>Subscriptions</p>
+                        <p>Talent Workshops</p>
+                        <p>CPSC Certificates</p>
+                        <p>Ratings</p>
+                        <p>Shop Help</p>
+                        <p>Contact Us</p>
+                    </div>
+                    <div>
+                        <h3>SITES</h3>
+                        <p>DC</p>
+                        <p>MAD Magazine</p>
+                        <p>DC Kids</p>
+                        <p>DC Universe</p>
+                        <p>DC Power Visa</p>
+                    </div>
+                </div>
+                <div class="footer-right">
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
